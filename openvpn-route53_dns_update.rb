75c981dd1556c9277d284241f43aaeecfa5d4ceb
Pawel J. Sawicki <github@pjs.name>
2011-01-04 15:33:11 +0100

    Aaa... and I thought that this time I'll get it right. :) I should have made my commit first, before pushing the source :)

--- openvpn-route53_dns_update.rb
+++ openvpn-route53_dns_update.rb
@@ -1,14 +1,15 @@
 #!/usr/bin/env ruby
 
 require 'net/http'
-require 'uri'
-require 'syslog'
 require 'route53'
+require 'syslog'
+require 'time'
+require 'uri'
 
 # config
-aws_access_key_id = 'AKIAIWMMPGNTMO44J7WA'
-aws_secret_access_key = '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'
-domain = 'vpn.careit.net.pl'
+aws_access_key_id = ''
+aws_secret_access_key = ''
+domain = ''
 
 Syslog.open('openvpn-route53_dns_update', Syslog::LOG_PID | Syslog::LOG_NDELAY, Syslog::LOG_DAEMON)
 
@@ -28,16 +29,28 @@ begin
 	zone = conn.get_zones(domain).first
 
 	if zone.nil?
-		Syslog.log(Syslog::LOG_INFO, "Unable to find the '#{zone}' zone.")
+		raise "Unable to find the '#{zone}' zone."
 	end
 
-	zone.get_records("A").each do |r|
+	zone.get_records("ANY").each do |r|
 		if r.name == fqdn
 			r.delete
 		end
 	end
 
 	record = Route53::DNSRecord.new(fqdn, "A", "60", [ip], zone)
+	response = record.create
+
+	if response.error?
+		raise response.error
+	else
+		Syslog.log(Syslog::LOG_INFO, "Updated DNS record for cn='#{cn}' with ip='#{ip}'.")
+	end
+
+	# v (vendor) = onoclea
+	# m (module) = aa ("at attention") ping-back-home software
+	# mo (module object) = vpn
+	record = Route53::DNSRecord.new(fqdn, "TXT", "60", ["\"v=onoclea m=aa mo=vpn cn:#{cn} ip:#{ip} ts:'#{Time.now.to_s}'\""], zone)
 	response = record.create
 
 	if response.error?
